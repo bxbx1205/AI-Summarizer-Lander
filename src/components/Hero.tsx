0e04c8fbf92742ea59bed5c7fd50641c81481326
Sarvesh Bhanarkar <srbhanarkar05@gmail.com>
2025-02-20 14:02:50 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,80 +1,163 @@
-import { Button } from "./ui/button";
-import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
-import { HeroCards } from "./HeroCards";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
-export const Hero = () => {
-  const [themeColor, setThemeColor] = useState("#9333EA"); // Default purple
-  const [hoverColor, setHoverColor] = useState("#A78BFA"); // Lighter purple for hover
+import {  buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "@/components/ui/card";
+import { Check, Linkedin } from "lucide-react";
+import { LightBulbIcon } from "./Icons";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
-  useEffect(() => {
-    // Function to determine if it's dark mode based on user preference
-    const isDarkMode = () =>
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-    // Update theme color based on dark mode
-    if (isDarkMode()) {
-      setThemeColor("#34D399"); // Green for dark mode
-      setHoverColor("#6EE7B7"); // Lighter green for hover
-    } else {
-      setThemeColor("#9333EA"); // Purple for light mode
-      setHoverColor("#A78BFA"); // Lighter purple for hover
-    }
-  }, []);
-
+export const HeroCards = () => {
   return (
-    <section className="container grid  lg:grid-cols-2 place-items-center py-10 md:py-8 gap-10">
-      <div className="text-center lg:text-start space-y-6">
-        <main className="text-5xl md:text-6xl font-bold">
-          <h1 className="inline">
-            <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
-              SemiColonError's
-            </span>{" "}
-            AI
-          </h1>{" "}
-          Intelligent Document Summarization For Efficient{" "}
-          <h2 className="inline">
-            <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-              Information
-            </span>{" "}
-            Extraction
-          </h2>
-        </main>
+    <div className="hidden lg:flex flex-row flex-wrap gap-8 relative w-[700px] h-[500px]">
+      {/* Testimonial */}
+      <Card className="absolute w-[340px] -top-[15px] drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+        <CardHeader className="flex flex-row items-center gap-4 pb-2">
+          <Avatar>
+            <AvatarImage
+              alt=""
+              src="https://upload.wikimedia.org/wikipedia/en/3/34/Jimmy_McGill_BCS_S3.png"
+            />
+            <AvatarFallback>SH</AvatarFallback>
+          </Avatar>
 
-        <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-          Unlock the Power of AI for Efficient Document Summarization
-        </p>
+          <div className="flex flex-col">
+            <CardTitle className="text-lg">Saul Goodman</CardTitle>
+            <CardDescription>@saulgoodman</CardDescription>
+          </div>
+        </CardHeader>
 
-        <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <motion.div
-            className="w-full md:w-2/3 lg:w-2/3"
-            whileHover={{ scale: 1.05, boxShadow: `0 0 20px ${hoverColor}` }} // Glow effect
-            transition={{ duration: 0.2 }}
-          >
-            <Button
-              className="w-full transition-all duration-300 text-white font-bold py-3 px-6 rounded-md shadow-md"
-              style={{
-                background: themeColor,
-              }}
-              onClick={() =>
-                (window.location.href =
-                  "https://ai-summarizer-delta-eosin.vercel.app/")
-              }
+        <CardContent>I summarize my files from here too!</CardContent>
+      </Card>
+
+      {/* Team */}
+      <Card className="absolute right-[20px] top-4 w-80 flex flex-col justify-center items-center drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+        <CardHeader className="mt-8 flex justify-center items-center pb-2">
+          <img
+            src="https://cdn.discordapp.com/attachments/1341687232855146565/1341706293492191232/SemiColonError_1.png?ex=67b6f8dc&is=67b5a75c&hm=c2308894cad530a5ac1c89498ec3805065321b96ca5c134a7a0be80fda8b464b&"
+            alt="user avatar"
+            className="absolute grayscale-[0%] -top-12 rounded-full w-24 h-24 aspect-square object-cover"
+          />
+          <CardTitle className="text-center">SemiColonError</CardTitle>
+         
+        </CardHeader>
+
+        <CardContent className="text-center pb-2">
+          <p>
+          We thrive on bringing ideas to life through innovative software solutions that go beyond expectations.
+          </p>
+        </CardContent>
+
+        <CardFooter>
+          <div>
+            <a
+              rel="noreferrer noopener"
+              href="https://github.com/"
+              target="_blank"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+              })}
             >
-              Get Started
-            </Button>
-          </motion.div>
-        </div>
-      </div>
+              <span className="sr-only">Github icon</span>
+              <GitHubLogoIcon className="w-5 h-5" />
+            </a>
+            <a
+              rel="noreferrer noopener"
+              href="https://twitter.com/"
+              target="_blank"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+              })}
+            >
+              <span className="sr-only">X icon</span>
+              <svg
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                className="fill-foreground w-5 h-5"
+              >
+                <title>X</title>
+                <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+              </svg>
+            </a>
 
-      {/* Hero cards sections */}
-      <div className="z-10">
-        <HeroCards />
-      </div>
+            <a
+              rel="noreferrer noopener"
+              href="https://www.linkedin.com"
+              target="_blank"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+              })}
+            >
+              <span className="sr-only">Linkedin icon</span>
+              <Linkedin size="20" />
+            </a>
+          </div>
+        </CardFooter>
+      </Card>
 
-      {/* Shadow effect */}
-      <div className="shadow"></div>
-    </section>
+      {/* Pricing */}
+      <Card className="absolute top-[150px] left-[50px] w-72  drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+        <CardHeader>
+          <CardTitle className="flex item-center justify-between">
+            Free
+            
+          </CardTitle>
+          <div>
+            <span className="text-3xl font-bold">Rs.0</span>
+            
+          </div>
+
+            <CardDescription>
+            Try our platform for free and experience enhanced efficiency across all features. No commitments required.
+            </CardDescription>
+        </CardHeader>
+
+      
+
+        <hr className="w-4/5 m-auto mb-4" />
+
+        <CardFooter className="flex">
+          <div className="space-y-4">
+            {["Summarize Anything", "Reliable", "Efficient"].map(
+              (benefit: string) => (
+                <span
+                  key={benefit}
+                  className="flex"
+                >
+                  <Check className="text-green-500" />{" "}
+                  <h3 className="ml-2">{benefit}</h3>
+                </span>
+              )
+            )}
+          </div>
+        </CardFooter>
+      </Card>
+
+      {/* Service */}
+       
+      <Card className="absolute w-[350px] -right-[10px] bottom-[35px] marginy-10 drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+        <CardHeader className="space-y-1 flex md:flex-row justify-start items-start gap-4">
+          <div className="mt-1 bg-primary/20 p-1 rounded-2xl">
+            <LightBulbIcon />
+          </div>
+          <div>
+            <CardTitle>Light & dark mode</CardTitle>
+            <CardDescription className="text-md mt-2">
+            Light mode makes you feel like you're living in a bright future, while dark mode feels like you're hiding from it.
+            </CardDescription>
+          </div>
+        </CardHeader>
+      </Card>
+    </div>
   );
 };
